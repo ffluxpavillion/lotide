83fbc676777c688ba6450207a4a05e15a09f98f1
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-02-11 16:03:57 -0500

    cleaned up a bit

--- head.js
+++ head.js
@@ -1,5 +1,4 @@
 // Original assertEqual function
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,10 +7,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// assertEqual TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+// head function
 const head = function(array) {
   return array [0];
 };
 
+// head TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
